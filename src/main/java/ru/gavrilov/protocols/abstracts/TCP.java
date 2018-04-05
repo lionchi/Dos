d6a5e6a861716c7df76756603a4bb70f8add61f3
GavrilovIvan <ivan.gavrilov@informicus.ru>
2018-04-05 23:22:52 +0300

    added base class

---
+++ src/main/java/ru/gavrilov/protocols/abstracts/TCP.java
@@ -0,0 +1,7 @@
+package ru.gavrilov.protocols.abstracts;
+
+/**
+ * Created by DDD on 05.04.2018.
+ */
+public class TCP {
+}
---
+++ src/main/java/ru/gavrilov/protocols/implement/DosUdp.java
@@ -0,0 +1,7 @@
+package ru.gavrilov.protocols.implement;
+
+/**
+ * Created by DDD on 05.04.2018.
+ */
+public class DosUdp {
+}
